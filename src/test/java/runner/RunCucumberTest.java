73a4c6b616fcdc39616a0cdf4fcff8f609646c59
Charlee <charleegithub@gmail.com>
2026-04-13 14:23:18 +0200

    test setup

---
+++ src/test/java/runner/RunCucumberTest.java
@@ -0,0 +1,20 @@
+package runner;
+
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.SNIPPET_TYPE_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "summary")
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase")
+public class RunCucumberTest {
+}
---
+++ src/test/java/steps/ProjectReportingSteps.java
@@ -0,0 +1,28 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProjectReportingSteps {
+    @Given("a project contains activities and time registrations")
+    public void aProjectContainsActivitiesAndTimeRegistrations() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the project manager generates a report")
+    public void theProjectManagerGeneratesAReport() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the system should display total time spent")
+    public void theSystemShouldDisplayTotalTimeSpent() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("remaining budgeted time")
+    public void remainingBudgetedTime() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
