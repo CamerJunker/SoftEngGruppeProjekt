d6857436caf0859286ea167049dddc832174b7e8
Camerjunker <ruthboazandersen@gmail.com>
2026-04-13 15:32:19 +0200

    Deleted old features folder and added register project step definitions

---
+++ src/test/java/steps/RegisterProjectSteps.java
@@ -0,0 +1,39 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegisterProjectSteps {
+    @Given("the employee is logged in")
+    public void theEmployeeIsLoggedIn() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Given("there exists a project with the name “Project1”")
+    public void thereExistsAProjectWithTheNameProject1() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Given("the project does not have a project manager")
+    public void theProjectDoesNotHaveAProjectManager() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Given("there exists an employee with the initials “HUBA”")
+    public void thereExistsAnEmployeeWithTheInitialsHUBA() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("the employee registers an employee with initials “HUBA” as project manager")
+    public void theEmployeeRegistersAnEmployeeWithInitialsHUBAAsProjectManager() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the project has “HUBA” as project manager")
+    public void theProjectHasHUBAAsProjectManager() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+m
+}
