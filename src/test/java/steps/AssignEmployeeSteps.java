7cff79fa01947d76ef7e2f5679b7e82cfab62c08
Abdullahx-z <156074235+Abdullahx-z@users.noreply.github.com>
2026-04-20 15:48:38 +0200

    AssignEmployeeSteps added

---
+++ src/test/java/steps/AssignEmployeeSteps.java
@@ -0,0 +1,59 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+class AssignEmployeeSteps {
+        @Given("the project {string} exists")
+        public void theProjectExists(String string) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Given("the project leader {string} exists")
+        public void theProjectLeaderExists(String string) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Given("the activity {string} exists")
+        public void theActivityExists(String string) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Given("the employee {string} exists")
+        public void theEmployeeExists(String string) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @When("the project leader assigns the employee {string} to the activity {string}")
+        public void theProjectLeaderAssignsTheEmployeeToTheActivity(String string, String string2) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Then("the employee should be assigned to the activity {string}")
+        public void theEmployeeShouldBeAssignedToTheActivity(String string) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Given("the employee {string} is already assigned to the activity {string}")
+        public void theEmployeeIsAlreadyAssignedToTheActivity(String string, String string2) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @Then("the system should show an error message")
+        public void theSystemShouldShowAnErrorMessage() {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        }
+
+        @When("the project leader assigns the employee {string} to a non existing activity")
+        public void theProjectLeaderAssignsTheEmployeeToANonExistingActivity(String string) {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+    }
+}
