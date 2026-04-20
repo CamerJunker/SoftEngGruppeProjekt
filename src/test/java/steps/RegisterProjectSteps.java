c5ef6f5dd9f968cc8a2e018057dd9862d34df400
Camerjunker <ruthboazandersen@gmail.com>
2026-04-20 16:42:44 +0200

    Removed and "m" in RegisterProjectSteps.java

--- src/test/java/steps/RegisterProjectSteps.java
+++ src/test/java/steps/RegisterProjectSteps.java
@@ -35,5 +35,42 @@ public class RegisterProjectSteps {
         // Write code here that turns the phrase above into concrete actions
         //throw new io.cucumber.java.PendingException();
     }
-m
+
+    @Given("an employee with initials {string}")
+    public void anEmployeeWithInitials(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("an activity {string} exists in project with serial number {string}")
+    public void anActivityExistsInProjectWithSerialNumber(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Given("the employee has registered {int} hours on the activity {string}")
+    public void theEmployeeHasRegisteredHoursOnTheActivity(Integer int1, String string) {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @When("the employee removes {int} hours from the activity {string}")
+    public void theEmployeeRemovesHoursFromTheActivity(Integer int1, String string) {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+    @Then("the system returns the error {string}")
+    public void theSystemReturnsTheError(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the employee registers a project with the name “Project1”")
+    public void theEmployeeRegistersAProjectWithTheNameProject1() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the project is registered to the project list")
+    public void theProjectIsRegisteredToTheProjectList() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
