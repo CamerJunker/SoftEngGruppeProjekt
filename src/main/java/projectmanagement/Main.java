72780befbb26d334deeb9af61c63e5a63caa855c
Camerjunker <ruthboazandersen@gmail.com>
2026-04-21 09:49:49 +0200

    Added user login methods to Main.java

--- src/main/java/projectmanagement/Main.java
+++ src/main/java/projectmanagement/Main.java
@@ -1,6 +1,40 @@
 package projectmanagement;
+
+import java.util.ArrayList;
+
 public class Main {
-    
+    private Boolean UserLoggedInFlag = false;
+    private User currentUser;
+    private ArrayList<User> ListOfUsers = new ArrayList<>();
+
+    public void loginUser(String initials){
+        if (searchUser(initials)){
+            this.UserLoggedInFlag = true;
+            this.currentUser = this.getUser(initials); 
+        }
+    }
+
+    // Check if user exist in preexisting list of users
+    private Boolean searchUser(String UserInitials){
+        for (User searchUser : this.ListOfUsers){
+            String searchUserInitials = searchUser.getName();
+            if (searchUserInitials.equals(UserInitials)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Get User as object, search by initials
+    private User getUser(String UserInitials){
+        for (User searchUser : this.ListOfUsers){
+            String searchUserInitials = searchUser.getName();
+            if (searchUserInitials.equals(UserInitials)){
+                return searchUser;
+            }
+        }
+        return null;
+    }
 }
 
 
