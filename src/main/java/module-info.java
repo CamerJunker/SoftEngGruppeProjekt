ff3e5787a2f1ac6e20704b86df954ac5becdae1b
Camerjunker <ruthboazandersen@gmail.com>
2026-04-22 06:39:18 +0200

    Added export of our folder for the main code, without it the maven compiler gives an error saying it's not exported.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,7 @@ module projectmanagement {
  
     opens dtu.example.ui to javafx.fxml; // Gives access to fxml files
     exports dtu.example.ui; // Exports the class inheriting from javafx.application.Application
+    
+    // Exports the class with the main code, otherwise the maven test will give an error saying that projectmanagement does not export to unnamed module [hasname].
+    exports projectmanagement;
 }
