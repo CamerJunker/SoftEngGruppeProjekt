0a1f2e6172bc3ee03f666f2d22ef03a50a340e30
Camerjunker <ruthboazandersen@gmail.com>
2026-05-09 15:28:40 +0200

    Whitebox tests are set up in folder "UnitTests"

---
+++ src/test/java/steps/UnitTests/DeleteProjectTest.java
@@ -0,0 +1,16 @@
+package steps.UnitTests;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Whitebox test: Delete Project Steps")
+public class DeleteProjectTest {
+    @Test
+    @DisplayName("Test!!!")
+    public void test(){
+        System.out.println("TESTING");
+        assertTrue(true, "Testing test");
+    }
+}
--- src/test/java/steps/UnitTests/TestJUnit5.java
+++ src/test/java/steps/UnitTests/TestJUnit5.java
@@ -1,4 +1,4 @@
-package steps;
+package steps.UnitTests;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
