045010b740f9ba9cb6e8b3deffa9253bca4b6ffc
Camerjunker <ruthboazandersen@gmail.com>
2026-05-09 15:41:15 +0200

    Moved unit tests to steps and renamed DeleteProjectTest to UnitDeleteProjectTest

--- src/test/java/steps/TestJUnit5.java
+++ src/test/java/steps/TestJUnit5.java
@@ -1,4 +1,4 @@
-package steps.UnitTests;
+package steps;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
--- src/test/java/steps/UnitDeleteProjectTest.java
+++ src/test/java/steps/UnitDeleteProjectTest.java
@@ -1,14 +1,21 @@
-package steps.UnitTests;
+package steps;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Whitebox test: Delete Project Steps")
-public class DeleteProjectTest {
+public class UnitDeleteProjectTest {
+
+    @BeforeEach
+    public void setUp(){
+        System.out.println("TEST");
+    }
+
     @Test
-    @DisplayName("Test!!!")
+    @DisplayName("Test 1")
     public void test(){
         System.out.println("TESTING");
         assertTrue(true, "Testing test");
